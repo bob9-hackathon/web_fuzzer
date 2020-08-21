9ea8f9a6162004cb2cf14a36c84b0538ded82b1d
jhyeon <pwn3rhyun@naver.com>
2020-08-21 22:32:55 +0900

    test

--- fuzzer.py
+++ fuzzer.py
@@ -1,9 +1,19 @@
 #!/usr/bin/python
 
-def intro():
-    print("################################################");
-    print("## Web Fuzzer ##");
-    print("박찬솔, 안평주, 석지원, 장재원, 정상훈, 정동현");
-    print("################################################");
 
-intro()
+class Fuzzer():
+
+    def intro():
+        print("################################################");
+        print("## Web Fuzzer ##");
+        print("박찬솔, 안평주, 석지원, 장재원, 정상훈, 정동현");
+        print("################################################");
+
+    
+    def __init__(self, url, seed, cookies={}):
+        self.url = url
+        self.seed = seed
+        self.cookies = {}
+
+if __name__ == "__main__":
+    Fuzzer(intro);
