81aacd864901b37f4515307930efdcb7d8d4c7de
JHye0n <stjhyeon@kakao.com>
2020-08-22 12:04:59 +0900

    Add files via upload

--- sql_fuzz_main.py
+++ sql_fuzz_main.py
@@ -7,7 +7,9 @@ if len(sys.argv) < 2:
     exit("[*] usage python3 sql_fuzz_main.py {url} {seedpath} {param}");
 
 url = sys.argv[1]
+
 seedpath = sys.argv[2]
+
 param = sys.argv[3]
 
 fuzzer = SQLFuzzer(url=url, seedpath=seedpath, param=param) 
