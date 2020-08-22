04327f45218990de594bf0ba44eb2fd2429bc881
JHye0n <stjhyeon@kakao.com>
2020-08-22 12:10:20 +0900

    Add files via upload
    param modify

--- sql_fuzz_main.py
+++ sql_fuzz_main.py
@@ -4,13 +4,11 @@ from fuzzer import *
 import sys
 
 if len(sys.argv) < 2:
-    exit("[*] usage python3 sql_fuzz_main.py {url} {seedpath} {param}");
+    exit("[*] usage python3 sql_fuzz_main.py {url} {seedpath}");
 
 url = sys.argv[1]
 
 seedpath = sys.argv[2]
-
-param = sys.argv[3]
 
 fuzzer = SQLFuzzer(url=url, seedpath=seedpath, param=param) 
 
